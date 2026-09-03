b2257cd3d19207570dd06b360bd6fc289e8c6d2e
unknown <mizanasif1@gmail.com>
2026-09-03 23:36:28 +0600

    cmnt krml

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Footer from "../Pages/Shared/Footer/Footer";
 import NavBar from '../Pages/Shared/NavBar/NavBar';
+// qr code scab problem 
 
 const RootLayout = () => {
     return (
